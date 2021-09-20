9694fe05d078ff373241912ac84aef49ad41ba40
claudio-evocorp <claudio@evocorp.com.br>
2021-09-20 14:06:22 -0300

    refactor(bot-discord): verificando se canal existe antes de enviar mensagem

--- src/bot-discord.ts
+++ src/bot-discord.ts
@@ -3,84 +3,87 @@ import {
 } from 'discord.js';
 
 interface StartParams {
-    tokenBot: string;
-    channelId: string;
-  }
+  tokenBot: string;
+  channelId: string;
+}
 
 export default class BotDiscord {
-    private static client: Client;
+  private static client: Client;
 
-    private static channel: TextChannel;
+  private static channel: TextChannel;
 
-    private static channelId: string;
+  private static channelId: string;
 
-    static start(params: StartParams) {
-      const { tokenBot, channelId } = params;
+  static start(params: StartParams) {
+    const { tokenBot, channelId } = params;
 
-      this.newClient();
-      this.onReady(() => this.setInfoChannel(channelId));
-      this.login(tokenBot);
-    }
+    this.newClient();
+    this.onReady(() => this.setInfoChannel(channelId));
+    this.login(tokenBot);
+  }
 
-    private static newClient(): Client {
-      this.client = new Client({
-        intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-      });
+  private static newClient(): Client {
+    this.client = new Client({
+      intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+    });
 
-      return this.client;
-    }
+    return this.client;
+  }
 
-    private static onReady(fncallback: () => void) {
-      this.client.on('ready', () => {
-        this.listenEvents();
-        fncallback();
-      });
-    }
+  private static onReady(fncallback: () => void) {
+    this.client.on('ready', () => {
+      this.listenEvents();
+      fncallback();
+    });
+  }
 
-    private static login(token: string) {
-      this.client.login(token);
-    }
+  private static login(token: string) {
+    this.client.login(token);
+  }
 
-    private static listenEvents() {
-      this.onMessageCreate();
-      this.onMessageDelete();
-      this.onMessageUpdate();
-    }
+  private static listenEvents() {
+    this.onMessageCreate();
+    this.onMessageDelete();
+    this.onMessageUpdate();
+  }
 
-    private static setInfoChannel(channelId: string) {
-      this.channelId = channelId;
-      this.channel = <TextChannel> this.client.channels.cache.get(this.channelId);
-    }
+  private static setInfoChannel(channelId: string) {
+    this.channelId = channelId;
+    this.channel = <TextChannel> this.client.channels.cache.get(this.channelId);
+  }
 
-    private static onMessageCreate() {
-      this.client.on('messageCreate', (message: Message) => {
-        console.log('Message created', message.content);
+  private static onMessageCreate() {
+    this.client.on('messageCreate', (message: Message) => {
+      console.log('Message created', message.content);
 
-        switch (message.content) {
-          case 'ping':
-            this.sendMessageText('Pong');
-            break;
+      switch (message.content) {
+        case 'ping':
+          this.sendMessageText('Pong');
+          break;
 
-          default:
-        }
-      });
-    }
+        default:
+      }
+    });
+  }
 
-    private static onMessageUpdate() {
-      this.client.on('messageUpdate', (oldMessage: Message, newMessage: Message) => {
-        console.log('Old message updated', oldMessage.content);
-        console.log('New message updated', newMessage.content);
-      });
-    }
+  private static onMessageUpdate() {
+    this.client.on('messageUpdate', (oldMessage: Message, newMessage: Message) => {
+      console.log('Old message updated', oldMessage.content);
+      console.log('New message updated', newMessage.content);
+    });
+  }
 
-    private static onMessageDelete() {
-      this.client.on('messageDelete', (message: Message) => {
-        console.log('Message deleted', message.content);
-      });
-    }
+  private static onMessageDelete() {
+    this.client.on('messageDelete', (message: Message) => {
+      console.log('Message deleted', message.content);
+    });
+  }
 
-    private static async sendMessageText(message: string) {
+  static async sendMessageText(message: string): Promise<Message | null> {
+    if (this.channel) {
       await this.channel.sendTyping();
-      this.channel.send(message);
+      return this.channel.send(message);
     }
+    return null;
+  }
 }
