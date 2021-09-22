14afce8baba94f68898e80bc3948814b31822066
claudiorodrigo <claudiorodrigozh@gmail.com>
2021-09-21 22:21:17 -0300

    fix(index): corrigindo index

--- src/app-discord.ts
+++ src/app-discord.ts
@@ -2,15 +2,36 @@ import {
   Client, Intents, Message, TextChannel,
 } from 'discord.js';
 
-interface SendMessageTextByChannelParams {
-  channelId: string;
+interface SendMessageBase {
   message: string;
+  pathsFiles?: string[];
 }
+
+interface SendMessageByChannel extends SendMessageBase {
+  channelId: string;
+}
+
+interface ReplyMessageByChannel extends SendMessageBase {
+  quoteMessage: Message;
+}
+
+interface PrintErrorParams {
+  message: string;
+  originalMessage: string;
+  stack: string;
+}
+
+// eslint-disable-next-line no-unused-vars
+type OnMessageCreateType = (message: Message) => void;
+
+// eslint-disable-next-line no-unused-vars
+type OnMessageUpdateType = (oldMessage: Message, newMessage: Message) => void;
+
+// eslint-disable-next-line no-unused-vars
+type OnMessageDeleteType = (message: Message) => void;
 
 export default class AppDiscord {
   private client: Client;
-
-  private isLoggedIn: boolean = false;
 
   constructor() {
     this.client = new Client({
@@ -18,26 +39,88 @@ export default class AppDiscord {
     });
   }
 
-  async login(tokenBot: string): Promise<void> {
+  async login(tokenBot: string): Promise<boolean> {
     try {
       await this.client.login(tokenBot);
-      this.isLoggedIn = true;
+      return true;
     } catch (error) {
-      this.isLoggedIn = false;
-      console.log('Erro ao efetuar login.');
+      this.printError({
+        message: 'Falha ao realizar login',
+        originalMessage: error.message,
+        stack: error.stack,
+      });
+
+      return false;
     }
   }
 
-  async sendMessageTextByChannel(params: SendMessageTextByChannelParams): Promise<Message | null> {
-    if (this.isLoggedIn) {
-      const { channelId, message } = params;
-      const channel = <TextChannel> this.client.channels.cache.get(channelId);
+  onReady(fnCallback: () => void): void {
+    this.client.on('ready', () => {
+      fnCallback();
+    });
+  }
 
-      if (channel) {
-        return channel.send(message);
-      }
+  onMessageCreate(fnCallback: OnMessageCreateType): void {
+    this.client.on('messageCreate', (message: Message) => {
+      fnCallback(message);
+    });
+  }
+
+  onMessageUpdate(fnCallback: OnMessageUpdateType): void {
+    this.client.on('messageUpdate', (oldMessage: Message, newMessage: Message) => {
+      fnCallback(oldMessage, newMessage);
+    });
+  }
+
+  onMessageDelete(fnCallback: OnMessageDeleteType): void {
+    this.client.on('messageDelete', (message: Message) => {
+      fnCallback(message);
+    });
+  }
+
+  async sendMessageByChannel(params: SendMessageByChannel): Promise<Message | null> {
+    try {
+      const { channelId, message, pathsFiles } = params;
+      const channel = this.getChannelById(channelId);
+      await channel.sendTyping();
+
+      return await channel.send({
+        content: message,
+        files: pathsFiles,
+      });
+    } catch (error) {
+      this.printError({
+        message: 'Falha ao enviar mensagem',
+        originalMessage: error.message,
+        stack: error.stack,
+      });
+      return null;
     }
+  }
 
-    return null;
+  async replyMessage(params: ReplyMessageByChannel): Promise<Message | null> {
+    try {
+      const { quoteMessage, message, pathsFiles } = params;
+
+      return await quoteMessage.reply({
+        content: message,
+        files: pathsFiles,
+      });
+    } catch (error) {
+      this.printError({
+        message: 'Falha ao responder mensagem',
+        originalMessage: error.message,
+        stack: error.stack,
+      });
+      return null;
+    }
+  }
+
+  getChannelById(channelId: string): TextChannel | null {
+    return <TextChannel> this.client?.channels?.cache?.get(channelId) || null;
+  }
+
+  private printError(params: PrintErrorParams): void {
+    console.log({ ...params });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
+import { Message } from 'discord.js';
 import { config } from 'dotenv';
 import AppDiscord from './app-discord';
-// import BotDiscord from './bot-discord';
 
 config();
 
-// BotDiscord.start({
-//   tokenBot: process.env.TOKEN_BOT,
-//   channelId: process.env.CHANNEL_ID,
-// });
+function start() {
+  const appDiscord = new AppDiscord();
 
-const appDiscord = new AppDiscord();
+  appDiscord.login(process.env.TOKEN_BOT);
 
-appDiscord.login(process.env.TOKEN_BOT);
+  appDiscord.onReady(async () => {
+    appDiscord.onMessageCreate(async (message: Message) => {
+      console.log('Message: ', message);
+    });
 
-setTimeout(() => {
-  appDiscord.sendMessageTextByChannel({
-    channelId: process.env.CHANNEL_ID,
-    message: 'Hello world',
+    appDiscord.onMessageDelete((message: Message) => {
+      console.log('Message deleted: ', message);
+    });
+
+    appDiscord.onMessageUpdate((message: Message) => {
+      console.log('Message updated: ', message);
+    });
   });
-}, 3000);
+}
+
+start();
